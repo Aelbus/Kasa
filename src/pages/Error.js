a13f5491554e79a9e8e79cee048f146df0ad9cee
Clement AeLBuS B <becker.clement@gmail.com>
2023-08-17 16:59:30 +0200

    Mise en place Page Error404

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,11 +1,11 @@
 import React from "react";
-//import "../styles/error404.css";
+import "../styles/Error404.css";
 import { Link } from "react-router-dom";
 
 const Error = () => {
   return (
-    <div className="error">
-      <div className="error-content">
+    <div className="error-div">
+      <div className="error-div-content">
         <p className="error-number">404</p>
         <p className="error-text">
           Oups! La page que vous demandez n'existe pas.
